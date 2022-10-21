ddd4871332f21bef81dae9bd413c92171a39ffde
Jasmine Graber <jasmine.n.graber@gmail.com>
2022-10-21 11:19:08 -0400

    initial commit

---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,37 @@
+package com.mindex.challenge.data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Compensation {
+    private Employee employee;
+    private String salary;
+    private LocalDate effectiveDate;
+
+    public Compensation() {
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(LocalDate effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+}
